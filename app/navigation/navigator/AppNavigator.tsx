418ab22e141c8a18ae8f0d9b33546fe92910ffb3
Tuan Anh Le <leanhtuanptithcm@gmail.com>
2021-01-26 15:46:45 +0700

    - remove context

--- app/navigation/navigator/AppNavigator.tsx
+++ app/navigation/navigator/AppNavigator.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthProvider } from "context/AuthContext";
 
 import { navigatorRef } from "../utils";
 
@@ -9,9 +8,7 @@ import MainScreen from "./Main";
 const App: React.FC<{}> = () => {
   return (
     <NavigationContainer ref={navigatorRef}>
-      <AuthProvider>
-        <MainScreen />
-      </AuthProvider>
+      <MainScreen />
     </NavigationContainer>
   );
 };
